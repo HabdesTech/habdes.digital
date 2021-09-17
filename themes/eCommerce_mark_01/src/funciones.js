b436c1b38659cca1c2fbcd01b4bc979e83c66c77
Letto <rodrigo0830@gmail.com>
2021-09-17 00:15:42 -0500

    add wp message function

--- themes/eCommerce_mark_01/src/funciones.js
+++ themes/eCommerce_mark_01/src/funciones.js
@@ -1,5 +1,5 @@
 //Esta sección sirve para crear el contador de productos
-const resultado = document.getElementById("contador")
+const cantidadProducto = document.getElementById("contador")
 const incrementar = document.getElementById("plusCircle")
 const reducir = document.getElementById("minusCircle")
 let cantidad = 1
@@ -12,15 +12,17 @@ reducir.addEventListener("click", () => {
     cantidad--
     modificarContador()
 })
-modificarContador = () => {    //Agregué una condicional IF para poder poner un limite al contador y que no se vuelva negativo
-    if( cantidad >= 1){   
-        resultado.textContent = cantidad
+modificarContador = () => {    
+    //Agregué una condicional IF para poder poner un limite al contador y que no se vuelva negativo
+    if (cantidad >= 1) {
+        cantidadProducto.textContent = cantidad
     } else {
         cantidad = 1
     }
 }
 
-//Sección
+//SECCION FORMULARIO
+//datos_del_usuario
 const nombreUsuario = document.getElementById("nombreUsuario");
 const telefonoUsuario = document.getElementById("telefonoUsuario");
 const dniUsuario = document.getElementById("dniUsuario");
@@ -28,42 +30,79 @@ const distritoUsuario = document.getElementById("distritoUsuario");
 const direccionUsuario = document.getElementById("direccionUsuario");
 const correoUsuario = document.getElementById("correoUsuario");
 const metodoEntrega = document.getElementById("metodoEntrega");
-const nombreProducto = document.getElementById("nombreProducto");
 const btnEnviar = document.getElementById("btnEnviarDatos");
+//datos_del_producto
+const nombreProducto = document.getElementById("nombreProducto");
+const precioProducto = document.getElementById("precioProducto");
+var urlProducto = window.location
+
+//datos_del_delivery
+let distritosCercanos = ["Barranco", "Chorrillos", "Lurín", "Miraflores", "Pachacamac", "Pueblo Libre", "San Borja", "San Isidro", "San Juan de Miraflores", "San Luis", "Santiago de Surco", "Surquillo", "Villa El Salvador", "Villa María del Triunfo"]
+let distritosMedianos = ["Ate", "Breña", "Independencia", "Jesús María", "La Molina", "La Victoria", "Lima", "Lince", "Magdalena del Mar", "Punta Hermosa", "Punta Negra", "San Juan de Lurigancho", "San Miguel", "Santa Anita", "Santa María del Mar"]
+let distritosLejanos = ["Ancón", "Carabayllo", "Chaclacayo", "Cieneguilla", "Comas", "El Agustino", "Los Olivos", "Lurigancho", "Pucusana", "Puente Piedra", "Rímac", "San Bartolo", "San Martín de Porres", "Santa Rosa"]
+var precioDelivery = "";
+
+//metodos_de_pago
+const metodoPago = document.getElementById("metodoPago")
+var formaPago;
+var precioTotal ;
+
 
 
 btnEnviar.addEventListener("click", () => {
-    location.href = `https://api.whatsapp.com/send/?phone=51960995232&text=Hola%2C+Me+interesa+el+siguiente+producto%3A%0AMi+nombre+es%3A+${nombreUsuario.value}%0AMi+DNI+es%3A+${dniUsuario.value}%0AMi+Teléfono+es%3A+${telefonoUsuario.value}%0AMi+Distrito+es%3A+${distritoUsuario.value}%0AVivo+en%3A+${direccionUsuario.value}%0AMi+correo+electrónico+es%3A+${correoUsuario.value}%0AMétodo+de+entrega%3A+${metodoEntrega.value}%0A%0ADetalle+del+producto%3A%0ANombre+del+producto%3A+${nombreProducto.textContent}%0ACantidad%3A+${resultado.textContent}`
+    //Con esta condicional se puede determinar que distrito eligió el usuario y determinar el precio de envio
+    if (metodoEntrega.value == "Recojo en local" ) {
+        precioDelivery = 0
+    } else {
+        for (var i = 0; i < distritosCercanos.length; i++) {
+            if (distritoUsuario.value == distritosCercanos[i]) {
+                precioDelivery = 5
+            } else if (distritoUsuario.value == distritosMedianos[i]) {
+                precioDelivery = 10
+            } else if (distritoUsuario.value == distritosLejanos[i]){
+                precioDelivery = 15
+            } else {
+                 "No aplica envío"
+            }
+        }
+    }
+    //Con esta condicional se puede determinar el método de pago y mostrar la información solicitada
+    if(metodoPago.value == "Transferencia interbancaria"){
+        formaPago = "Transferencia bancaria" + " " + 2003151735911
+    } else if (metodoPago.value == "Yape"){
+        formaPago = "Yapea" + " -> " + 993722873
+    } else if (metodoPago.value == "Plin"){
+        formaPago = "Plinea" + " -> " + 993722873
+    } else if (metodoPago.value == "tunki"){
+        formaPago = "Tunkea" + " -> " + 993722873
+    } else {
+        "No aplica"
+    }
+    //Con esta operación realizamos la operación aritmética para calcular el precio total del pedido. Se usa el "parseInt" para convertir los datos en números
+    precioTotal = (parseInt(precioProducto.textContent) + precioDelivery) * parseInt(cantidadProducto.textContent);
+
+    location.href = `https://api.whatsapp.com/send/?phone=51960995232&text=%F0%9F%91%8B%+Hola%21+Acabo+de+realizar+el+siguiente+pedido+en+https://digital.habdes.pe/%3A%0A%0A%F0%9F%91%A9%F0%9F%8F%BB%E2%80%8D%F0%9F%92%BBDATOS+DEL+CLIENTE%3A%0AMi+nombre+es%3A+${nombreUsuario.value}%0AMi+DNI+es%3A+${dniUsuario.value}%0AMi+Teléfono+es%3A+${telefonoUsuario.value}%0AMi+Distrito+es%3A+${distritoUsuario.value}%0AMi+dirección+en%3A+${direccionUsuario.value}%0AMi+correo+electrónico+es%3A+${correoUsuario.value}%0AMétodo+de+entrega%3A+${metodoEntrega.value}%0A%0A%F0%9F%93%A6+DETALLE+DEL+PEDIDO%3A%0ALink+del+producto%3A+${urlProducto}%0ANombre+del+producto%3A+${nombreProducto.textContent}%0ACantidad%3A+${cantidadProducto.textContent}%0ASubtotal%3A+S/.+${precioProducto.textContent}%0AEnvío%3A+S/.+${precioDelivery}%0ATotal%3A+S/.+${precioTotal}%0A%0A%F0%9F%92%B3+MÉTODO+DE+PAGO%3A%0AMétodo+de+pago+solicitado%3A+${formaPago}`
 })
 
-// location.href = `https://api.whatsapp.com/send/?phone=51960995232&text=Hola, mi nombre es ${nombreUsuario} vivo en ${direccionCliente} y quiero ${cantidadProducto} ${nombreProducto}, gracias.`
+// %F0%9F%91%8B%+Hola%21+Acabo+de+realizar+el+siguiente+pedido+en+https://digital.habdes.pe/%3A%0A%0A
+// DATOS+DEL+CLIENTE%3A%0A
+// Mi+nombre+es%3A+${nombreUsuario.value}%0A
+// Mi+DNI+es%3A+${dniUsuario.value}%0A
+// Mi+Teléfono+es%3A+${telefonoUsuario.value}%0A
+// Mi+Distrito+es%3A+${distritoUsuario.value}%0A
+// Mi+dirección+en%3A+${direccionUsuario.value}%0A
+// Mi+correo+electrónico+es%3A+${correoUsuario.value}%0A
+// Método+de+entrega%3A+${metodoEntrega.value}%0A%0A
+// DETALLE+DEL+PEDIDO%3A%0A
+// Link+del+producto%3A+${urlProducto}%0A
+// Nombre+del+producto%3A+${nombreProducto.textContent}%0A
+// Cantidad%3A+${cantidadProducto.textContent}%0A
+// Subtotal%3A+S/.+${precioProducto.textContent}%0A
+// Envío%3A+S/.+${precioDelivery}%0A
+// Total%3A+S/.+${precioTotal}%0A%0A
+// MÉTODO+DE+PAGO%3A%0A
+// Método+de+pago+solicitado%3A+${formaPago}
 
-// Hola%21+Acabo+de+realizar+el+siguiente+pedido%3A%0A
-// Nombre%3A++51993722873+Aybar%0A
-// DNI%3A+71336104%0A
-// Pedido%3A+OR-24836D64%0A
-// M%C3%A9todo+de+entrega%3A+Delivery%0A
-// Direcci%C3%B3n%3A+Av+Pachacutec+2717+%28Casa%29%0A
-// Lima%2C+Lima%2C+Villa+Maria+del+Triunfo%0A
-// Correo%3A+rodrigo0830%40gmail.com%0A
-// %0A
-// %EF%BF%BD+Bar+ecol%C3%B3gico+colgante++-+1+un.+-+S%2F+280.00++-+http%3A%2F%2Fecopractico.tiendada.com%2Fproduct%2FPR-495A2574%0A
-// %EF%BF%BD+Subtotal%3A+S%2F+280.00%0A
-// %EF%BF%BD+Env%C3%ADo%3A+--%0A
-// %EF%BF%BD+Total%3A+Por+confirmar%0A
-
-// Hola%2C+Me+interesa+el+siguiente+producto%3A%0A
-// Mi+nombre+es%3A+${nombreUsuario}%0A
-// Mi+DNI+es%3A+${dniUsuario}%0A
-// Mi+Teléfono+es%3A+${telefonoUsuario}%0A
-// Mi+Distrito+es%3A+${distritoUsuario}%0A
-// Vivo+en%3A+${direccionUsuario}%0A
-// Mi+correo+electrónico+es%3A+${correoUsuario}%0A
-// Método+de+entrega%3A+${metodoEntrega}%0A
-// %0A
-// Detalle+del+producto%3A%0A
-// Nombre+del+producto%3A+${nombreProducto}%0A
-// Cantidad%3A+${resultado}
 
 
 
